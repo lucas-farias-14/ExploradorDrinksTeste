1b9396ab5adbb95e248a89dac7cf8162cad8e204
Molsuco <molsuco@hotmail.com>
2025-04-29 22:47:18 -0300

    adicionado header

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,11 @@
 </script>
 
 <template>
-
+  <div class="header">
+    <div class="title-header">
+      <h1 class="title">Explorador de Drinks</h1>
+      <p>Escolha o seu favorito!</p>
+    </div>
+  </div>
   <RouterView />
 </template>
